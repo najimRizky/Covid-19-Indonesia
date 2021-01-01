2ad52191911d0c716b2b03959266c53a57117a47
Najim Rizky <najim.rizky@student.umn.ac.id>
2021-01-01 11:17:50 +0700

    Home, Fetch, Grid, Paper

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+//import axios from 'axios'
 
 function Home() {
-    
+    const [item, setItem] = useState([]);
+    const [fetchStatus, setFetchStatus] = useState(false);
+
+    useEffect(() => {
+        fetchItems();
+        
+    },[])
+
+    const fetchItems = async () => {
+        const data = await fetch('https://apicovid19indonesia-v2.vercel.app/api/indonesia/more');
+        const items = await data.json();
+        console.log(items)
+        setItem(items);
+        setFetchStatus(true)
+    }
+
     return (
         <div>
-            <h1>Home</h1>
+            {fetchStatus ? (
+            <>
+                <h1>Total:</h1>
+                <Grid container xs={12} justify="center" spacing={3} >
+                    <Grid item xs={3} >
+                        <Paper>
+                            <h2>Positif: {item.total.positif}</h2>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3} >
+                        <Paper>
+                            <h2>Sembuh: {item.total.sembuh}</h2>
+                        </Paper>
+                    </Grid>
+                </Grid>
+                <h1>Pertambahan:</h1>
+                <h2>Kasus Baru: +{item.penambahan.positif}</h2>
+                <h2>Sembuh: +{item.penambahan.sembuh}</h2>
+                <h3>Last Update: {item.penambahan.tanggal}</h3>
+            </>
+            ):
+            (   <h2>Loading..</h2>
+            )
+            }
         </div>
     );
 }
 
 export default Home;
 
-/*const options = {
-    method: 'GET',
-    url: 'https://covid-19-data.p.rapidapi.com/report/country/name',
-    params: {date: '2020-04-01', name: 'Italy'},
-    headers: {
-      'x-rapidapi-key': '1d8a3f39camsh83451addabc2b2dp10c818jsn30d0f7e63639',
-      'x-rapidapi-host': 'covid-19-data.p.rapidapi.com'
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-      console.log(response.data);
-  }).catch(function (error) {
-      console.error(error);
-  });*/
+/*
+
+*/
 
 
 
